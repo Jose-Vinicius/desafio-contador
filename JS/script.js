2a15f564d5e240d4f726cbd244beb30da6af8380
Jose vinicius francener sarmento <70924237+Jose-Vinicius@users.noreply.github.com>
2021-04-19 13:56:56 -0300

    desafio

--- JS/script.js
+++ JS/script.js
@@ -4,15 +4,63 @@ document.getElementById('calcular').onclick = function (){calcular();}
 //funções
 
 function calcular(){
-    let pNumero = Number(document.getElementById('inicialNumber').value)
-    let fNumero = Number(document.getElementById('finalNumber').value)
-    let pularNumero = Number(document.getElementById('puloNumber').value)
-
-    for( i = pNumero; i < fNumero; i + pularNumero){
-        console.log(i)
-    }
-}
-
-function loop(){
+    let primeiroNumero = Number(document.getElementById('inicialNumber').value);
+    let ultimoNumero = Number(document.getElementById('finalNumber').value);
+    let numeroDePulo = Number(document.getElementById('puloNumber').value);
+    let resultado = document.getElementById('resultado');
     
+
+    if(primeiroNumero === ""){
+        resultado.innerHTML = '<h2>Por favor insira um valor valido no primeiro numero!</h2>';
+    } else if(ultimoNumero === ''){
+        resultado.innerHTML = '<h2>Por favor insira um valor valido no numero final!</h2>';
+    } else if(ultimoNumero <= primeiroNumero ){
+        resultado.innerHTML = '<h2>Os valores do ultimo numero não pode ser menor ou igual ao do numero inicial</h2>';
+    } else if(numeroDePulo === ''){
+        resultado.innerHTML = '<h2>Por favor insira um valor valido no numero de pulos!</h2>';
+    } else if(numeroDePulo == 0){
+        numeroDePulo = 1;
+        loop();
+    } else {
+        loop();
+    }
+
+    function loop(){
+        
+        resultado.innerHTML = "";
+        for( let i = primeiroNumero; i <= ultimoNumero; i += numeroDePulo){
+            resultado.innerHTML += `<span> ${i}, </span>`;
+        };
+        document.getElementById('inicialNumber').value = "";
+        document.getElementById('finalNumber').value = "";
+        document.getElementById('puloNumber').value = "";
+    }
+
+
+    let valoresDosresultados = [];
+
+    let valoresDosInputs = [
+            {primeiroNumero: `${primeiroNumero}`,
+            ultimoNumero: `${ultimoNumero}`, 
+            numeroDePulo: `${numeroDePulo}`},
+        ];
+    
+    valoresDosresultados.push(valoresDosInputs);
+
+    let valoresDeSaida = [];
+
+    for(let i = primeiroNumero; i <= ultimoNumero; i += numeroDePulo){
+        valoresDeSaida.push(i)
+    }
+
+    valoresDosresultados.push(valoresDeSaida)
+
+    console.log(valoresDosresultados);
+
+    localStorage.setItem('valoresDosResultados', JSON.stringify(valoresDosresultados));
+
+    let = itemSalvo = localStorage.getItem('valoresDosResultados');
+
+    console.log('itemSalvo:', JSON.parse(itemSalvo))
 }
+
